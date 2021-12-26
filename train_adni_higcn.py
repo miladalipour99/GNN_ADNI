9a8d8dead93b302e3de767882319acc0c9e54fc7
miladalipour99 <60439091+miladalipour99@users.noreply.github.com>
2021-12-26 14:22:24 -0800

    Update train_adni_higcn.py

--- train_adni_higcn.py
+++ train_adni_higcn.py
@@ -70,11 +70,11 @@ def load_time_series():
 
             tmp_list = []
             for line in f:  # 116
-                tmp = np.zeros(116)
+                tmp = np.zeros(106)
                 line.strip('\n')
                 line = line.split(' ')
 
-                for columns in range(116):
+                for columns in range(106):
                     tmp[columns] = line[columns]
 
                 tmp_list.append(tmp)
